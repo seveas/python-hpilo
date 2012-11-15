aa9d98b1ac74cfd296291002baaff76df9233540
Nathan Garabedian <ngarabedian@apple.com>
2012-11-15 14:15:57 -0800

    mod_dir_config updated to allow DIR_KERBEROS_KEYTAB property gracefully

--- hpilo.py
+++ hpilo.py
@@ -120,7 +120,7 @@ class Ilo(object):
             xml = b("\r\n").join(etree.tostringlist(xml)) + b('\r\n')
         else:
             xml = etree.tostring(xml)
-        print xml
+
         header, data =  self._communicate(xml, self.protocol, progress=progress)
 
         # This thing usually contains multiple XML messages
@@ -883,7 +883,7 @@ class Ilo(object):
             dir_user_context_15=None,dir_enable_grp_acct=None,
             dir_kerberos_enabled=None,dir_kerberos_realm=None,
             dir_kerberos_kdc_address=None,dir_kerberos_kdc_port=None,
-            #dir_kerberos_keytab=None,
+            dir_kerberos_keytab=None,
             dir_grpacct1_name=None,dir_grpacct1_sid=None,
             dir_grpacct1_priv=None,dir_grpacct2_name=None,
             dir_grpacct2_sid=None,dir_grpacct2_priv=None,
@@ -897,9 +897,16 @@ class Ilo(object):
              will be changed."""
         vars = dict(locals())
         del vars['self']
+
+        # create special case for element with text inside
+        keytab_el = etree.Element('DIR_KERBEROS_KEYTAB')
+        keytab_el.text = dir_kerberos_keytab
+        del vars['dir_kerberos_keytab']
+
         elements = [etree.Element(x.upper(), VALUE=str({True: 'Yes', \
                 False: 'No'}.get(vars[x], vars[x])))
                     for x in vars if vars[x] is not None]
+        elements.append(keytab_el)
         return self._control_tag('DIR_INFO','MOD_DIR_CONFIG',elements=elements)
 
 
