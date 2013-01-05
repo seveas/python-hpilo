8895f7b250cd03a08863152547a54ce5eb6c8bce
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2013-01-05 18:00:17 +0100

    Release bugfix version 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = "python-hpilo",
-      version = "2.0",
+      version = "2.0.1",
       author = "Dennis Kaarsemaker",
       author_email = "dennis@kaarsemaker.net",
       url = "http://github.com/seveas/python-hpilo",
