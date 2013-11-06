273bb14b21c8bceea5771b5703744ea1dd1b7fb1
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2013-11-06 20:36:19 +0100

    Typo: s/dmci/dcmi/

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = "python-hpilo",
-      version = "2.4",
+      version = "2.4.1",
       author = "Dennis Kaarsemaker",
       author_email = "dennis@kaarsemaker.net",
       url = "http://github.com/seveas/python-hpilo",
