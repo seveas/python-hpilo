5c09d55e947de131d0d1624b80bfae1f88bc01a5
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2014-08-23 21:01:36 +0200

    New logo

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ html_theme_path = [csp.get_theme_dir()]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/lightbulb.png'
+html_logo = '_static/python-hpilo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
