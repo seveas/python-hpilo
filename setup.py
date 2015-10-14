3c48821c66f9c730507aff0c38385fb0f17c4319
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-10-14 21:14:35 +0200

    Version 3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = "python-hpilo",
-      version = "3.2",
+      version = "3.3",
       author = "Dennis Kaarsemaker",
       author_email = "dennis@kaarsemaker.net",
       url = "http://github.com/seveas/python-hpilo",
